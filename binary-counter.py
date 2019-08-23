b72ea2ea9365782e8a22487df06c4c64673efa6a
ronan <g00342393@gmit.ie>
2019-08-23 13:12:58 +0100

    Added led setting code

--- binary-counter.py
+++ binary-counter.py
@@ -3,11 +3,22 @@ import unicornhathd
 
 import threading
 from datetime import datetime
+import math
+
+unicornhathd.rotation(0)
+unicornhathd.brightness(0.6)
+u_width, u_height = unicornhathd.get_shape()
 
 def update_counter():
     threading.Timer(1.0, update_counter).start()
 
-    seconds = (datetime(2020, 6, 1) - datetime.now()).total_seconds()
-    print(seconds)
+    seconds = math.ceil((datetime(2020, 6, 1) - datetime.now()).total_seconds())
+    bin_seconds = str(bin(seconds))[2:]
+
+    unicornhathd.clear()
+    for i in range(len(bin_seconds)):
+        x, y = i % u_width, i / u_width
+        unicornhathd.set_pixel(x, y, 255, 255, 255)
+
 
 update_counter()
